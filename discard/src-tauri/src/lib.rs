757a074a61bdd622a28574efa03c79d0fd42e26c
Bret Amerman <bret.amerman@icloud.com>
2025-10-29 02:45:31 +0000

    update the plugin

--- discard/src-tauri/src/lib.rs
+++ discard/src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ use tauri_plugin_updater::UpdaterExt;
 pub fn run() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|app| {
             let handle = app.handle().clone();
             tauri::async_runtime::spawn(async move {
